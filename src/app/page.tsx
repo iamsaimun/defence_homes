b50a72a88723d99b3b8a333b62aebd048728761f
MD. Saimun Rahman <saimunmail@gmail.com>
2025-01-03 23:41:01 +0600

    add text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@ import { ClientForm } from "@/components/form";
 export default function Home() {
   return (
     <>
-      <main className=" min-h-svh content-center">
+      <main className=" min-h-svh content-center bg-gray-900">
+        <section className="max-w-sm m-auto pb-10">
+          <h1 className="font-black text-5xl text-white">
+            Real State Selling Share
+          </h1>
+          <h2 className="text-lg text-white">Defence Homes</h2>
+        </section>
         <section>
           <ClientForm />
         </section>
